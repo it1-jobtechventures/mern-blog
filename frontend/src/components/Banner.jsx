4ce8f12c22796b1180111677fc385654e28465df
it <it1.jobtechventure.com>
2024-12-17 16:23:25 +0530

    add caraousel

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -1,37 +1,43 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const Banner = () => {
-  const [banner, setBanner] = useState([]);
+    const [banner, setBanner] = useState([]);
 
-  const fetchBanner = async () => {
-    try {
-        const response = await axios.get('http://localhost:4000/api/banner/listBanner'); 
-        if (response.data.success) {
-            setBanner(response.data.data);
-            console.log("banner" ,response.data)
-        } else {
-            console.error('Error fetching banner');
+    const fetchBanner = async () => {
+        try {
+            const response = await axios.get('http://localhost:4000/api/banner/listBanner');
+            if (response.data.success) {
+                setBanner(response.data.data);
+            } else {
+                console.error('Error fetching banner');
+            }
+        } catch (error) {
+            console.error('Error fetching banner:', error);
         }
-    } catch (error) {
-        console.error('Error fetching banner:', error);
-    }
+    };
+
+    useEffect(() => {
+        fetchBanner();
+    }, []);
+
+  return (
+    <div className="border border-yellow-900 w-52 mb-3">
+        <Carousel showArrows={true} infiniteLoop={true} autoPlay={true} interval={3000} showThumbs={false}dynamicHeight={true}>
+        {
+            banner.map((ban, id) => (
+                <div key={id}>
+                    <a href={ban.link} target="_blank" rel="noopener noreferrer">
+                        <img src={`http://localhost:4000/images/${ban.image}`} alt="banner" className="max-w-full h-auto"/>
+                    </a>
+                </div>
+            ))
+        }
+        </Carousel>
+    </div>
+  );
 };
 
-useEffect(() => {
-fetchBanner()
-},[])
-  return (
-    <>
-<div className='border border-yellow-900 m-3 p-5'>
-    {banner.map((ban, id) => (
-        <a key={id} href={ban.link} target='_blank' rel='noopener noreferrer'>
-            <img src={`http://localhost:4000/images/${ban.image}`} alt='banner' className='max-w-full h-auto'/>
-        </a>
-    ))}
-</div>
-    </>
-  )
-}
-
-export default Banner
+export default Banner;
