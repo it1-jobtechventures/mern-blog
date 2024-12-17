bf721118f96788a1b9a2923bd6416edab33a38af
it <it1.jobtechventure.com>
2024-12-17 11:29:35 +0530

    display detail of selected blog

--- frontend/src/components/BlogDetail.jsx
+++ frontend/src/components/BlogDetail.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom'
 import blogData from '../assets/blogData'
+import axios from 'axios';
 
 const BlogDetail = () => {
   const { id } = useParams(); 
-  const blog = blogData[id];
+//   const blog = blogData[id];
+  const [blog,setBlog] = useState(null)
+
+  const fetchBlog = async () => {
+    try {
+      const response = await axios.post(`http://localhost:4000/api/blog/blog/${id}`); // Adjust your API URL accordingly
+      if (response.data.success) {
+        setBlog(response.data.data); // Set the fetched blog to the state
+      } else {
+        console.error('Error fetching the blog');
+      }
+    } catch (error) {
+      console.error('Error fetching the blog:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchBlog()
+  },[id])
 
     if (!blog) {
         return <div>Blog not found</div>;
@@ -14,9 +33,9 @@ const BlogDetail = () => {
     <> 
         <div className="p-8 pt-16">
             <h2 className="text-4xl font-bold">{blog.title}</h2>
-            <p className="text-sm text-gray-500 mt-2">{blog.date_published}</p>
+            <p className="text-sm text-gray-500 mt-2">{new Date(blog.date).toLocaleDateString()}</p>
             <h3 className="text-2xl font-semibold mt-4">{blog.headline}</h3>
-            <img src={blog.image} alt={blog.title} className="w-full h-80 object-cover mt-4 rounded-lg border" />
+            <img src={`http://localhost:4000/images/${blog.image}`} alt={blog.title} className="w-full h-80 object-cover mt-4 rounded-lg border" />
             <p className="text-lg mt-4">{blog.description}</p>
             <div className="mt-6">
             {
