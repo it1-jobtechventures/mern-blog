39febd58160efeb1c7efae8686a8ac5a51ca11f0
it <it1.jobtechventure.com>
2024-12-18 11:05:51 +0530

    add link to the images

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -9,19 +9,19 @@ const Logo = () => {
     <> 
         <div className='flex justify-around items-center pb-7'>
             <div className='flex flex-col items-center'>
-                <img src={alanze} alt='logo' className='h-28 w-28 border p-1'/>
+                <a href='https://alanze.in/' target='_blank'><img src={alanze} alt='logo' className='h-28 w-28 border p-1'/></a>
                 <p> Online retail Apparel and Fashion Platform</p>
             </div>
             <div className='flex flex-col items-center'>
-                <img src={ArfectLogo} alt='logo' className='h-28 w-28 border p-1'/>
+                <a href='https://arfect.com/' target='_blank'><img src={ArfectLogo} alt='logo' className='h-28 w-28 border p-1'/></a>
                 <p>Medical Affairs And scientific Communication Firm</p>
             </div>
             <div className='flex flex-col items-center'>
-                <img src={hindco} alt='logo' className='h-28 w-28 border p-1'/>
+                <a href='https://www.hindco.com/' target='_blank'><img src={hindco} alt='logo' className='h-28 w-28 border p-1'/></a>
                 <p>Executive search And  Recruitment consulting Firm</p>
             </div>
             <div className='flex flex-col items-center'>
-                <img src={jobringer} alt='logo' className='h-28 w-28 border p-1'/>
+                <a href='https://jobringer.com/' target='_blank'><img src={jobringer} alt='logo' className='h-28 w-28 border p-1'/></a>
                 <p>JobRinger~India's job portal</p>
             </div>
         </div>
