19d3b8d98a77958a21b27f29c2d35e358c7b7fc7
it <it1.jobtechventure.com>
2024-12-18 11:05:11 +0530

    fetch imges

--- frontend/src/components/Gallery.jsx
+++ frontend/src/components/Gallery.jsx
@@ -1,22 +1,36 @@
-
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 
 const Gallery = () => {
+  const [photo, setPhotos] = useState([]);
+
+  const fetchGallery = async () => {
+    try {
+      const response = await axios.get('http://localhost:4000/api/gallery/listPhoto');
+      if (response.data.success) {
+        setPhotos(response.data.data);
+        console.log(response.data)
+      } else {
+        console.error('Error fetching photos');
+      }
+    } catch (error) {
+      console.error('Error fetching photos:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchGallery();
+  }, []);
+
   return (
     <div className='h-screen bg-purple-500 pt-16 '>
       <div className='grid grid-cols-4 gap-5 p-4'>
-      <img src='' alt='' className='border h-40' />
-      <img src='' alt='' className='border h-40' />
-      <img src='' alt='' className='border h-40' />
-      <img src='' alt='' className='border h-40' />
-      <img src='' alt='' className='border h-40' />
-      <img src='' alt='' className='border h-40' />
-      <img src='' alt='' className='border h-40' />
-      <img src='' alt='' className='border h-40' />
-      <img src='' alt='' className='border h-40' />
-      <img src='' alt='' className='border h-40' />
+        {
+          photo.map((pht,idx) => (
+            <img src={`http://localhost:4000/images/${pht.image}`} alt="gallery" className="max-w-full h-auto"/>
+          ))
+        }
       </div>
-
     </div>
   )
 }
