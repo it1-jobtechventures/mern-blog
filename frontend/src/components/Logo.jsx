7290d4abc16147bf929c50695ef561ae3cc26a4b
it <it1.jobtechventure.com>
2024-12-19 10:21:02 +0530

    change path

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -34,85 +34,96 @@
 
 
 
-import React from 'react';
-import { AiOutlineRight } from 'react-icons/ai'; 
-import alanze from '../assets/alanze.png';
-import ArfectLogo from '../assets/ArfectLogo.png';
-import hindco from '../assets/hindco.png';
-import jobringer from '../assets/jobringer.png';
+// import React from 'react';
+// import { AiOutlineRight } from 'react-icons/ai'; 
+// import alanze from '../assets/alanze.png';
+// import ArfectLogo from '../assets/ArfectLogo.png';
+// import hindco from '../assets/hindco.png';
+// import jobringer from '../assets/jobringer.png';
+
+// const Logo = () => {
+//   return (
+//     <div className="flex flex-col md:flex-row gap-3 justify-center pb-7 px-4">
+//       {/* Logo 1 */}
+//       <div className="flex flex-col items-center group relative w-full sm:w-1/2 lg:w-1/4">
+//         <a href="https://alanze.in/" target="_blank" rel="noopener noreferrer">
+//           <img
+//             src={alanze}
+//             alt="alanze"
+//             className="w-48 h-32 object-fit border p-3 transition-transform duration-300 ease-in-out group-hover:scale-100"
+//           />
+//         </a>
+//         <div className="absolute bottom-0 left-0 right-0 text-center p-2 bg-black bg-opacity-70 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+//           <p>Online retail Apparel and Fashion Platform</p>
+//           <a href="https://alanze.in/" target="_blank" rel="noopener noreferrer">
+//             <AiOutlineRight className="inline-block ml-2 text-xl" />
+//           </a>
+//         </div>
+//       </div>
+
+//       {/* Logo 2 */}
+//       <div className="flex flex-col items-center group relative w-full sm:w-1/2 lg:w-1/4">
+//         <a href="https://arfect.com/" target="_blank" rel="noopener noreferrer">
+//           <img
+//             src={ArfectLogo}
+//             alt="ArfectLogo"
+//             className="w-48 h-32 object-fit border p-3 transition-transform duration-300 ease-in-out group-hover:scale-100"
+//           />
+//         </a>
+//         <div className="absolute bottom-0 left-0 right-0 text-center p-2 bg-black bg-opacity-70 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+//           <p>Medical Affairs And Scientific Communication Firm</p>
+//           <a href="https://arfect.com/" target="_blank" rel="noopener noreferrer">
+//             <AiOutlineRight className="inline-block ml-2 text-xl" />
+//           </a>
+//         </div>
+//       </div>
+
+//       {/* Logo 3 */}
+//       <div className="flex flex-col items-center group relative w-full sm:w-1/2 lg:w-1/4">
+//         <a href="https://www.hindco.com/" target="_blank" rel="noopener noreferrer">
+//           <img
+//             src={hindco}
+//             alt="hindco"
+//             className="w-48 h-32 object-fit border p-3 transition-transform duration-300 ease-in-out group-hover:scale-100"
+//           />
+//         </a>
+//         <div className="absolute bottom-0 left-0 right-0 text-center p-2 bg-black bg-opacity-70 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+//           <p>Executive Search And Recruitment Consulting Firm</p>
+//           <a href="https://www.hindco.com/" target="_blank" rel="noopener noreferrer">
+//             <AiOutlineRight className="inline-block ml-2 text-xl" />
+//           </a>
+//         </div>
+//       </div>
+
+//       {/* Logo 4 */}
+//       <div className="flex flex-col items-center group relative w-full sm:w-1/2 lg:w-1/4">
+//         <a href="https://jobringer.com/" target="_blank" rel="noopener noreferrer">
+//           <img
+//             src={jobringer}
+//             alt="jobringer"
+//             className="w-48 h-32 object-fit border p-3 transition-transform duration-300 ease-in-out group-hover:scale-100"
+//           />
+//         </a>
+//         <div className="absolute bottom-0 left-0 right-0 text-center p-2 bg-black bg-opacity-70 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+//           <p>JobRinger ~ India's Job Portal</p>
+//           <a href="https://jobringer.com/" target="_blank" rel="noopener noreferrer">
+//             <AiOutlineRight className="inline-block ml-2 text-xl" />
+//           </a>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Logo;
+
+
+import React from 'react'
 
 const Logo = () => {
   return (
-    <div className="flex flex-col md:flex-row gap-3 justify-center pb-7 px-4">
-      {/* Logo 1 */}
-      <div className="flex flex-col items-center group relative w-full sm:w-1/2 lg:w-1/4">
-        <a href="https://alanze.in/" target="_blank" rel="noopener noreferrer">
-          <img
-            src={alanze}
-            alt="alanze"
-            className="w-48 h-32 object-fit border p-3 transition-transform duration-300 ease-in-out group-hover:scale-100"
-          />
-        </a>
-        <div className="absolute bottom-0 left-0 right-0 text-center p-2 bg-black bg-opacity-70 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-          <p>Online retail Apparel and Fashion Platform</p>
-          <a href="https://alanze.in/" target="_blank" rel="noopener noreferrer">
-            <AiOutlineRight className="inline-block ml-2 text-xl" />
-          </a>
-        </div>
-      </div>
+    <div>Logo</div>
+  )
+}
 
-      {/* Logo 2 */}
-      <div className="flex flex-col items-center group relative w-full sm:w-1/2 lg:w-1/4">
-        <a href="https://arfect.com/" target="_blank" rel="noopener noreferrer">
-          <img
-            src={ArfectLogo}
-            alt="ArfectLogo"
-            className="w-48 h-32 object-fit border p-3 transition-transform duration-300 ease-in-out group-hover:scale-100"
-          />
-        </a>
-        <div className="absolute bottom-0 left-0 right-0 text-center p-2 bg-black bg-opacity-70 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-          <p>Medical Affairs And Scientific Communication Firm</p>
-          <a href="https://arfect.com/" target="_blank" rel="noopener noreferrer">
-            <AiOutlineRight className="inline-block ml-2 text-xl" />
-          </a>
-        </div>
-      </div>
-
-      {/* Logo 3 */}
-      <div className="flex flex-col items-center group relative w-full sm:w-1/2 lg:w-1/4">
-        <a href="https://www.hindco.com/" target="_blank" rel="noopener noreferrer">
-          <img
-            src={hindco}
-            alt="hindco"
-            className="w-48 h-32 object-fit border p-3 transition-transform duration-300 ease-in-out group-hover:scale-100"
-          />
-        </a>
-        <div className="absolute bottom-0 left-0 right-0 text-center p-2 bg-black bg-opacity-70 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-          <p>Executive Search And Recruitment Consulting Firm</p>
-          <a href="https://www.hindco.com/" target="_blank" rel="noopener noreferrer">
-            <AiOutlineRight className="inline-block ml-2 text-xl" />
-          </a>
-        </div>
-      </div>
-
-      {/* Logo 4 */}
-      <div className="flex flex-col items-center group relative w-full sm:w-1/2 lg:w-1/4">
-        <a href="https://jobringer.com/" target="_blank" rel="noopener noreferrer">
-          <img
-            src={jobringer}
-            alt="jobringer"
-            className="w-48 h-32 object-fit border p-3 transition-transform duration-300 ease-in-out group-hover:scale-100"
-          />
-        </a>
-        <div className="absolute bottom-0 left-0 right-0 text-center p-2 bg-black bg-opacity-70 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-          <p>JobRinger ~ India's Job Portal</p>
-          <a href="https://jobringer.com/" target="_blank" rel="noopener noreferrer">
-            <AiOutlineRight className="inline-block ml-2 text-xl" />
-          </a>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default Logo;
+export default Logo
