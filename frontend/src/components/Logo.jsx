9253cbbbe3cbc3a478ff8a8721183f12f4fb7e2c
it <it1.jobtechventure.com>
2024-12-19 12:06:44 +0530

    change ui

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -1,128 +1,33 @@
-// import React from 'react'
-// import alanze from '../assets/alanze.png'
-// import ArfectLogo from '../assets/ArfectLogo.png'
-// import hindco from '../assets/hindco.png'
-// import jobringer from '../assets/jobringer.png'
-
-// const Logo = () => {
-//   return (
-//     <> 
-//         <div className='flex  pb-7'>
-//             <div className='flex flex-col items-center '>
-//                 <a href='https://alanze.in/' target='_blank'><img src={alanze} alt='logo' className=' w-60 h-20 object-fit border p-1'/></a>
-//                 <p> Online retail Apparel and Fashion Platform</p>
-//             </div>
-//             <div className='flex flex-col items-center '>
-//                 <a href='https://arfect.com/' target='_blank'><img src={ArfectLogo} alt='logo' className=' w-60 h-20  object-fit border p-1'/></a>
-//                 <p>Medical Affairs And scientific Communication Firm</p>
-//             </div>
-//             <div className='flex flex-col items-center '>
-//                 <a href='https://www.hindco.com/' target='_blank'><img src={hindco} alt='logo' className=' w-60 h-20 object-fit border p-1'/></a>
-//                 <p>Executive search And  Recruitment consulting Firm</p>
-//             </div>
-//             <div className='flex flex-col items-center '>
-//                 <a href='https://jobringer.com/' target='_blank'><img src={jobringer} alt='logo' className=' w-60 h-20 object-fit border p-1'/></a>
-//                 <p>JobRinger~India's job portal</p>
-//             </div>
-//         </div>
-//     </>
-//   )
-// }
-
-// export default Logo
-
-
-
-
-// import React from 'react';
-// import { AiOutlineRight } from 'react-icons/ai'; 
-// import alanze from '../assets/alanze.png';
-// import ArfectLogo from '../assets/ArfectLogo.png';
-// import hindco from '../assets/hindco.png';
-// import jobringer from '../assets/jobringer.png';
-
-// const Logo = () => {
-//   return (
-//     <div className="flex flex-col md:flex-row gap-3 justify-center pb-7 px-4">
-//       {/* Logo 1 */}
-//       <div className="flex flex-col items-center group relative w-full sm:w-1/2 lg:w-1/4">
-//         <a href="https://alanze.in/" target="_blank" rel="noopener noreferrer">
-//           <img
-//             src={alanze}
-//             alt="alanze"
-//             className="w-48 h-32 object-fit border p-3 transition-transform duration-300 ease-in-out group-hover:scale-100"
-//           />
-//         </a>
-//         <div className="absolute bottom-0 left-0 right-0 text-center p-2 bg-black bg-opacity-70 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-//           <p>Online retail Apparel and Fashion Platform</p>
-//           <a href="https://alanze.in/" target="_blank" rel="noopener noreferrer">
-//             <AiOutlineRight className="inline-block ml-2 text-xl" />
-//           </a>
-//         </div>
-//       </div>
-
-//       {/* Logo 2 */}
-//       <div className="flex flex-col items-center group relative w-full sm:w-1/2 lg:w-1/4">
-//         <a href="https://arfect.com/" target="_blank" rel="noopener noreferrer">
-//           <img
-//             src={ArfectLogo}
-//             alt="ArfectLogo"
-//             className="w-48 h-32 object-fit border p-3 transition-transform duration-300 ease-in-out group-hover:scale-100"
-//           />
-//         </a>
-//         <div className="absolute bottom-0 left-0 right-0 text-center p-2 bg-black bg-opacity-70 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-//           <p>Medical Affairs And Scientific Communication Firm</p>
-//           <a href="https://arfect.com/" target="_blank" rel="noopener noreferrer">
-//             <AiOutlineRight className="inline-block ml-2 text-xl" />
-//           </a>
-//         </div>
-//       </div>
-
-//       {/* Logo 3 */}
-//       <div className="flex flex-col items-center group relative w-full sm:w-1/2 lg:w-1/4">
-//         <a href="https://www.hindco.com/" target="_blank" rel="noopener noreferrer">
-//           <img
-//             src={hindco}
-//             alt="hindco"
-//             className="w-48 h-32 object-fit border p-3 transition-transform duration-300 ease-in-out group-hover:scale-100"
-//           />
-//         </a>
-//         <div className="absolute bottom-0 left-0 right-0 text-center p-2 bg-black bg-opacity-70 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-//           <p>Executive Search And Recruitment Consulting Firm</p>
-//           <a href="https://www.hindco.com/" target="_blank" rel="noopener noreferrer">
-//             <AiOutlineRight className="inline-block ml-2 text-xl" />
-//           </a>
-//         </div>
-//       </div>
-
-//       {/* Logo 4 */}
-//       <div className="flex flex-col items-center group relative w-full sm:w-1/2 lg:w-1/4">
-//         <a href="https://jobringer.com/" target="_blank" rel="noopener noreferrer">
-//           <img
-//             src={jobringer}
-//             alt="jobringer"
-//             className="w-48 h-32 object-fit border p-3 transition-transform duration-300 ease-in-out group-hover:scale-100"
-//           />
-//         </a>
-//         <div className="absolute bottom-0 left-0 right-0 text-center p-2 bg-black bg-opacity-70 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-//           <p>JobRinger ~ India's Job Portal</p>
-//           <a href="https://jobringer.com/" target="_blank" rel="noopener noreferrer">
-//             <AiOutlineRight className="inline-block ml-2 text-xl" />
-//           </a>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Logo;
-
-
-// import React from 'react'
-
+import React from 'react'
+import logo1 from '../assets/jobringer.png'
 const Logo = () => {
   return (
-    <div>Logo</div>
+    <div className="flex justify-center items-center border ">
+      <div className="flex justify-center items-center h-full">
+        <div className="grid grid-cols-2 h-full">
+          <div>
+            <img src={logo1} alt="image" />
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint totam nam voluptatum animi, nesciunt soluta veniam </p>
+            <p>Links</p>
+          </div>
+          <div>
+            <img src="" alt="image" />
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint totam nam voluptatum animi, nesciunt soluta veniam </p>
+            <p>Links</p>
+          </div>
+          <div>
+            <img src="" alt="image" />
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint totam nam voluptatum animi, nesciunt soluta veniam </p>
+            <p>Links</p>
+          </div>
+          <div>
+            <img src="" alt="image" />
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint totam nam voluptatum animi, nesciunt soluta veniam </p>
+            <p>Links</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
