c9548165a1ca27d976896c33c22f656ba303675b
it <it1.jobtechventure.com>
2024-12-19 17:37:16 +0530

    logo file

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -37,48 +37,48 @@
 
 // export default Logo
 
-  // import React from "react";
-  // import jobringer from "../assets/jobringer.png";
-  // import alanze from "../assets/alanze.png";
-  // import hindco from "../assets/hindco.png";
-  // import arfectLogo from "../assets/arfectLogo.png";
-
-  // const Logo = () => {
-  //   return (
-  //     <div className="p-4">
-  //       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-  //         {[jobringer, alanze, hindco, arfectLogo].map((logo, idx) => (
-  //           <div
-  //             key={idx}
-  //             className="rounded-md border border-black p-4 flex flex-col items-center"
-  //           >
-  //             <img
-  //               className="rounded-sm h-32 w-full object-contain bg-white"
-  //               src={logo}
-  //               alt="logo"
-  //             />
-  //             <p className="text-center mt-2">
-  //               Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-  //             </p>
-  //             <a href="/" className="mt-2">
-  //               <button className="bg-[#ff6200] mt-1 px-4 py-2 rounded-lg text-white">
-  //                 VISIT
-  //               </button>
-  //             </a>
-  //           </div>
-  //         ))}
-  //       </div>
-  //     </div>
-  //   );
-  // };
-
-  // export default Logo;
-  import React from 'react'
+  import React from "react";
+  import jobringer from "../assets/jobringer.png";
+  import alanze from "../assets/alanze.png";
+  import hindco from "../assets/hindco.png";
+  import arfectLogo from "../assets/arfectLogo.png";
 
   const Logo = () => {
     return (
-      <div>Logo</div>
-    )
-  }
+      <div className="p-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          {[jobringer, alanze, hindco, arfectLogo].map((logo, idx) => (
+            <div
+              key={idx}
+              className="rounded-md border border-black p-4 flex flex-col items-center"
+            >
+              <img
+                className="rounded-sm h-32 w-full object-contain bg-white"
+                src={logo}
+                alt="logo"
+              />
+              <p className="text-center mt-2">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              </p>
+              <a href="/" className="mt-2">
+                <button className="bg-[#ff6200] mt-1 px-4 py-2 rounded-lg text-white">
+                  VISIT
+                </button>
+              </a>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  };
 
-  export default Logo
+  export default Logo;
+  // import React from 'react'
+
+  // const Logo = () => {
+  //   return (
+  //     <div>Logo</div>
+  //   )
+  // }
+
+  // export default Logo
