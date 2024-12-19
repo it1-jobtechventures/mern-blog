bb9883802940188132e56f05de83490a574df058
it <it1.jobtechventure.com>
2024-12-19 10:48:09 +0530

    change blogo omponent

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -1,35 +1,35 @@
-import React from 'react'
-import alanze from '../assets/alanze.png'
-import ArfectLogo from '../assets/ArfectLogo.png'
-import hindco from '../assets/hindco.png'
-import jobringer from '../assets/jobringer.png'
+// import React from 'react'
+// import alanze from '../assets/alanze.png'
+// import ArfectLogo from '../assets/ArfectLogo.png'
+// import hindco from '../assets/hindco.png'
+// import jobringer from '../assets/jobringer.png'
 
-const Logo = () => {
-  return (
-    <> 
-        <div className='flex  pb-7'>
-            <div className='flex flex-col items-center '>
-                <a href='https://alanze.in/' target='_blank'><img src={alanze} alt='logo' className=' w-60 h-20 object-fit border p-1'/></a>
-                <p> Online retail Apparel and Fashion Platform</p>
-            </div>
-            <div className='flex flex-col items-center '>
-                <a href='https://arfect.com/' target='_blank'><img src={ArfectLogo} alt='logo' className=' w-60 h-20  object-fit border p-1'/></a>
-                <p>Medical Affairs And scientific Communication Firm</p>
-            </div>
-            <div className='flex flex-col items-center '>
-                <a href='https://www.hindco.com/' target='_blank'><img src={hindco} alt='logo' className=' w-60 h-20 object-fit border p-1'/></a>
-                <p>Executive search And  Recruitment consulting Firm</p>
-            </div>
-            <div className='flex flex-col items-center '>
-                <a href='https://jobringer.com/' target='_blank'><img src={jobringer} alt='logo' className=' w-60 h-20 object-fit border p-1'/></a>
-                <p>JobRinger~India's job portal</p>
-            </div>
-        </div>
-    </>
-  )
-}
+// const Logo = () => {
+//   return (
+//     <> 
+//         <div className='flex  pb-7'>
+//             <div className='flex flex-col items-center '>
+//                 <a href='https://alanze.in/' target='_blank'><img src={alanze} alt='logo' className=' w-60 h-20 object-fit border p-1'/></a>
+//                 <p> Online retail Apparel and Fashion Platform</p>
+//             </div>
+//             <div className='flex flex-col items-center '>
+//                 <a href='https://arfect.com/' target='_blank'><img src={ArfectLogo} alt='logo' className=' w-60 h-20  object-fit border p-1'/></a>
+//                 <p>Medical Affairs And scientific Communication Firm</p>
+//             </div>
+//             <div className='flex flex-col items-center '>
+//                 <a href='https://www.hindco.com/' target='_blank'><img src={hindco} alt='logo' className=' w-60 h-20 object-fit border p-1'/></a>
+//                 <p>Executive search And  Recruitment consulting Firm</p>
+//             </div>
+//             <div className='flex flex-col items-center '>
+//                 <a href='https://jobringer.com/' target='_blank'><img src={jobringer} alt='logo' className=' w-60 h-20 object-fit border p-1'/></a>
+//                 <p>JobRinger~India's job portal</p>
+//             </div>
+//         </div>
+//     </>
+//   )
+// }
 
-export default Logo
+// export default Logo
 
 
 
@@ -120,10 +120,10 @@ export default Logo
 
 // import React from 'react'
 
-// const Logo = () => {
-//   return (
-//     <div>Logo</div>
-//   )
-// }
+const Logo = () => {
+  return (
+    <div>Logo</div>
+  )
+}
 
-// export default Logo
+export default Logo
