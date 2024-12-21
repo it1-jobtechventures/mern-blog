7f1fca0402d25dabd62390ffd11d0b77bc62ec78
it <it1.jobtechventure.com>
2024-12-21 11:36:46 +0530

    add new field

--- backend/models/blogModel.js
+++ backend/models/blogModel.js
@@ -4,11 +4,12 @@ const blogSchema = new mongoose.Schema({
     title: { type: String, required: true },
     headline: { type: String, required: true },
     // description: { type: String, required: true },
-    long_description: { type: String, required: true },
+    // long_description: { type: String, required: true },
     image: { type: String, required: true }, 
     images: { type: [String]},
     keyword: { type: [String], required: true },
     category: { type: String },
+    content: { type: String, required: true },
     date: { type: Date, default: Date.now }
 });
 
