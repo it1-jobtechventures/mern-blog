5ec5ee07dd776d31601181aca2e9d266fd63458c
it <it1.jobtechventure.com>
2024-12-21 11:29:25 +0530

    change field

--- backend/models/blogModel.js
+++ backend/models/blogModel.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const blogSchema = new mongoose.Schema({
     title: { type: String, required: true },
     headline: { type: String, required: true },
-    description: { type: String, required: true },
+    // description: { type: String, required: true },
     long_description: { type: String, required: true },
     image: { type: String, required: true }, 
     images: { type: [String]},
