c91a210a6de92cecdf4c2ba1e5bc9e771ca831bc
it <it1.jobtechventure.com>
2024-12-24 14:42:48 +0530

    change send email

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -46,6 +46,7 @@ const Contact = ({ url }) => {
         toast.error("Error submitting form.");
       }
     } catch (error) {
+      console.log(error.message)
       toast.error("Error submitting form.");
     }
   };
@@ -61,16 +62,17 @@ const Contact = ({ url }) => {
 
     emailjs
       .send(
-        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_SERVICE_ID, 
         import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
         templateParams,
-        import.meta.env.VITE_EMAILJS_USER_ID
+        import.meta.env.VITE_EMAILJS_PUBLIC_KEY
       )
       .then(
         (response) => {
           toast.success("Email sent successfully");
         },
         (error) => {
+          console.log(error.message)
           toast.error("Error sending email");
         }
       );
