acd4d03cc0d44592d48bf6390a5f7d8c64c900a6
it <it1.jobtechventure.com>
2024-12-26 15:03:26 +0530

    created routes for comment and like

--- backend/routes/blogRoute.js
+++ backend/routes/blogRoute.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { addBlog, listBlog, removeBlog ,getBlockById, updateBlog} from "../controllers/blogController.js";
 import upload from "../middleware/multer.js";
+import {  addComment, getComments, likeBlog } from "../controllers/likeCommentController.js";
 
 const blogRouter = express.Router();
 
@@ -9,5 +10,8 @@ blogRouter.get('/list' ,listBlog)
 blogRouter.post('/remove' ,removeBlog)
 blogRouter.post('/blog/:id' ,getBlockById)
 blogRouter.put("/update/:id", upload.single("image"), updateBlog);
+blogRouter.post("/like/:id", likeBlog); // Like a blog
+blogRouter.post('/comment/:id', addComment);// Add a comment to a blog post
+blogRouter.get('/comments/:id', getComments);// Get comments for a specific blog post
 
 export default blogRouter;
