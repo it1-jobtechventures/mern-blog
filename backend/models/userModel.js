5bce6cedc35e2d8c82511282573a938459bfa1d8
it <it1.jobtechventure.com>
2024-12-27 13:00:39 +0530

    update user model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,14 +1,15 @@
 // create user model 
 import mongoose from 'mongoose'
 
-
 // user Schema
 const userSchema = new mongoose.Schema({
     name:{type:String,required:true},
     email:{type:String,required:true,unique:true},
     password:{type:String,required:true},
+    createdAt: { type: Date, default: Date.now },
 },{minimize:false})
 
 // create model in database
 const userModel = mongoose.model.user || mongoose.model("user" ,userSchema)
 export default userModel;
+
