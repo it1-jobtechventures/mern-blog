966d164083c76901dd82387713e3eae670c86cca
it <it1.jobtechventure.com>
2024-12-27 13:12:25 +0530

    add text

--- frontend/src/assets/logoData.js
+++ frontend/src/assets/logoData.js
@@ -7,6 +7,7 @@ import jobtech from '/jobtech.png'
 const logoData = [
     {
         "image":jonringer,
+        "text":"JOBRINGER ~ India's Job Portal, a robust and feature-rich Job Portal, designed to cater the industry needs in current market scenario, equipped with latest technology and AI based algorithms.",
         "linkedin":"https://in.linkedin.com/company/jobringer",
         "insta":"https://www.instagram.com/jobringerjobs",
         "facebook":"https://www.facebook.com/jobwithjobringer/",
@@ -14,6 +15,7 @@ const logoData = [
     },
     {
         "image":arfectLogo,
+        "text":"ARFECT ~ Medical & Scientific Communications Firm, caters to supporting pharmaceutical industry for their business needs especially designing and delivering end-to-end project management towards brand growth and brand promotion.",
         "linkedin":"https://in.linkedin.com/company/arfect",
         "insta":"https://www.instagram.com/arfect.business.solutions/",
         "facebook":"https://www.facebook.com/profile.php?id=100090887163544&mibextid=ZbWKwL",
@@ -21,6 +23,7 @@ const logoData = [
     },
     {
         "image":hindco,
+        "text":"HINDCO, a leading Executive Search & Career Coaching Firm of India, deals in search, selection and placement of senior and middle level professionals for the Industry.",
         "linkedin":"https://in.linkedin.com/company/hindco",
         "insta":"https://www.instagram.com/hindcojobs/?hl=en",
         "facebook":"https://www.facebook.com/HindcoConsultingServices/",
@@ -28,6 +31,7 @@ const logoData = [
     },
     {
         "image":alanze,
+        "text":"ALANZE, a Retail Apparel and Fashion company with a wide range of products distributed thru online platforms.",
         "linkedin":"https://in.linkedin.com/company/alanze",
         "insta":"https://www.instagram.com/alanzefashion/#",
         "facebook":"https://www.facebook.com/alanzefashion/",
@@ -35,7 +39,8 @@ const logoData = [
     },
     {
         "image":jobtech,
-        "linkedin":"",
+        "text":"",
+        "linkedin":"https://www.linkedin.com/authwall?trk=bf&trkInfo=AQHU0C6lac445wAAAZQGz5kAQaKPsFvNv-4WOs-3lJe-Vk0yCQcOIaK2BITzpczX5vQv-f9kp5FvmtytzS_jy2lifvU5rY9cwWXWElb_HJ8kHpi7wsZf0e9Ia1JKy4Bo_fFpFTY=&original_referer=&sessionRedirect=https%3A%2F%2Fwww.linkedin.com%2Fcompany%2Fjobtechventures",
         "insta":"",
         "facebook":"",
         "twitter":""
