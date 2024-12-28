388ddd843f0b7e165e9229bde50b107adef8dfdb
it <it1.jobtechventure.com>
2024-12-28 13:16:27 +0530

    update model to required false for category

--- backend/models/blogModel.js
+++ backend/models/blogModel.js
@@ -5,7 +5,7 @@ const blogSchema = new mongoose.Schema({
     headline: { type: String },
     image: { type: String, required: true }, 
     keyword: { type: [String], required: true },
-    category: { type: mongoose.Schema.Types.ObjectId, ref: "category" },
+    category: { type: mongoose.Schema.Types.ObjectId, ref: "category" , required:false },
     content: { type: String, required: true },
     date: { type: Date, default: Date.now },
     likes: { type: Number, default: 0 },
