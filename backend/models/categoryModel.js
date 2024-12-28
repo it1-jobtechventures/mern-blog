e29c44d5283ee2f6e7fdbb54c667e8744d213237
it <it1.jobtechventure.com>
2024-12-28 11:53:13 +0530

    model for category

---
+++ backend/models/categoryModel.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose'
+
+// user Schema
+const categorySchema = new mongoose.Schema({
+    name:{type:String,required:true ,unique: true},
+},{minimize:false})
+
+// create model in database
+const categoryModel = mongoose.model.category || mongoose.model("category" ,categorySchema)
+export default categoryModel;
