95db6f4299c71f2f8a6e3f8da51427f365068470
it <it1.jobtechventure.com>
2024-12-30 16:43:23 +0530

    update display url and add skeleton

--- frontend/src/components/Gallery.jsx
+++ frontend/src/components/Gallery.jsx
@@ -8,7 +8,7 @@ const Gallery = ({ url }) => {
 
   const fetchGallery = async () => {
     try {
-      setLoading(true)
+      setLoading(true);
       const response = await axios.get(`${url}/api/gallery/listPhoto`);
       if (response.data.success) {
         setPhotos(response.data.data);
@@ -18,8 +18,8 @@ const Gallery = ({ url }) => {
     } catch (error) {
       toast.error('Error fetching photos. Please try again.');
       console.error('Error fetching photos:', error);
-    }finally{
-      setLoading(false)
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -28,26 +28,31 @@ const Gallery = ({ url }) => {
   }, []);
 
   return (
-    loading ? (
-      <div className="flex justify-center items-center">
-        <div className="w-6 h-6 border-t-2 border-b-2 border-white rounded-full animate-spin"></div>
-      </div>
-    ) : (
-      <div className="pt-16 pb-10 bg-[#202020]">
-        <div className="container mx-auto px-4">
-          <h2 className="text-3xl text-white font-semibold text-center mb-8">Gallery</h2>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-            {photo.map((pht, idx) => (
-              <div key={idx} className="relative group">
-                <img src={pht.image} alt="gallery" className="w-full h-64 object-cover rounded-lg transition-all duration-300 ease-in-out transform group-hover:scale-105 group-hover:shadow-lg"/>
+    <div className="pt-16 pb-10 bg-[#202020]">
+      <div className="container mx-auto px-4">
+        <h2 className="text-3xl text-white font-semibold text-center mb-8">Gallery</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          {loading
+            ? Array.from({ length: 8 }).map((_, index) => (
+              <div key={index} className="w-full h-64 bg-gray-700 rounded-lg animate-pulse"/>
+            ))
+            : photo.map((item, index) => (
+              <div key={index}>
+                {item.type === 'video' ? (
+                  <video controls className="w-full h-64 object-cover rounded-lg transition-all duration-300 ease-in-out transform group-hover:scale-105 group-hover:shadow-lg">
+                    <source src={item.media} type="video/mp4" />
+                    Your browser does not support the video tag.
+                  </video>
+                  ) : (
+                    <img src={item.media} alt="media" className="w-full h-64 object-cover rounded-lg transition-all duration-300 ease-in-out transform group-hover:scale-105 group-hover:shadow-lg"/>
+                  )}
               </div>
-            ))}
-          </div>
+            )
+          )}
         </div>
       </div>
-    )
+    </div>
   );
 };
 
 export default Gallery;
-
