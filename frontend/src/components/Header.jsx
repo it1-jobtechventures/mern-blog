2c167e9210131a133d03752a6756e6b1deca1405
it <it1.jobtechventure.com>
2025-01-08 15:53:54 +0530

    change text

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -30,7 +30,7 @@ const Header = () => {
         </div> */}
       </header>
       <div>
-        <header className="flex flex-col-reverse md:flex-row">
+        <header className="flex flex-col-reverse md:flex-row ">
           <div className="w-full md:w-1/2 flex justify-center  p-6">
             <img src={bioImage} alt="profile" className="rounded-md shadow-lg w-full max-w-sm md:max-w-md lg:max-w-lg h-auto"/>
           </div>
@@ -41,19 +41,13 @@ const Header = () => {
               </h1>
               <div className="w-full sm:w-1/4 h-1 bg-[#ff9724]"></div>
             </div>
-            <p className="text-base sm:text-lg leading-relaxed mb-4 pt-7 p-6 sm:p-10 lg:p-14">
-              Dr Orestis Georgiou is an author of 6 patents, 2 books, and 100+
-              academic papers that have been published in leading journals and
-              conferences of Mathematics, Physics, Computer Science, Engineering
-              and Medicine. He has also been co-awarded R&D grants in excess of
-              €10 million, including a Marie Curie Individual Fellowship, and is
-              the recipient of the prestigious 2019 IEEE Heinrich Hertz award.
-              Finally, as Head of R&D Partnerships at Ultraleap, he is passionate
-              about haptic technologies, networks, and spatial XR computing and
-              oversees all our external R&D collaboration activities and
-              grant-funded projects.
+            <p className="text-base sm:text-lg leading-relaxed mb-4 pt-4 sm:p-10  text-justify">
+              <p>I’m <span className="font-bold">Prakash Bansal</span>, an entrepreneur and business leader with a deep passion for driving innovation and scaling businesses. Over the years, I’ve successfully built and led ventures across multiple industry sectors, leveraging technology and strategic vision to create impactful, high-growth solutions.<br/></p>
+              <p className="pt-3">Passionate about turning bold ideas into sustainable businesses that not only meet market needs but also lead through innovation. My entrepreneurial journey is rooted in the belief that impactful businesses are built through customer-centric solutions, operational excellence, and strong leadership.<br/></p>
+              <p className="pt-3">From launching startups to leading high-growth companies, I have always focused on fostering a culture of innovation and delivering real value. I like to interact with people from diverse backgrounds, any generation of entrepreneurs, sharing insights and strategies for business growth and leadership.</p>
+              <p className="pt-3">Let’s connect to explore how we can innovate, collaborate, and grow together!</p>
             </p>
-            <div className="-mt-6 pl-6 sm:pl-10 lg:pl-14 text-lg sm:text-xl">
+            <div className=" pl-6 sm:pl-10 lg:pl-14 text-lg sm:text-xl">
               <Link to={"/about"}>
                 <button className="bg-[#ff9724] text-white p-3 rounded-full">
                   About me
