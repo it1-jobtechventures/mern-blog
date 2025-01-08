f3db444fdb62053ace9b99352844d9225b277781
it <it1.jobtechventure.com>
2025-01-08 11:11:18 +0530

    change css

--- frontend/src/components/UpdatedLink.jsx
+++ frontend/src/components/UpdatedLink.jsx
@@ -34,9 +34,9 @@ const UpdatedLink = ({ url }) => {
       <div className="p-5 m-3">
         <h1 className="text-2xl text-black text-center md:text-3xl font-bold mb-2">Updated List</h1>
         <div className=" bottom-0 left-0 w-3/4 h-1 bg-[#ff9724]"></div>
-        <div className="w-full md:w-96 h-72 overflow-y-auto scrollbar-hide rounded-md p-3 mt-6">
+        <div className="w-full  h-72 overflow-y-auto scrollbar-hide rounded-md p-3 mt-6">
           {allUpdateList.map((list, id) => (
-            <div key={id} className="border border-b-2 border-black text-black mt-6 p-3 mb-2 rounded-md hover:shadow-md transition-shadow" >
+            <div key={id} className="border border-b-2  border-black text-black  p-3 mb-2 rounded-md hover:shadow-md transition-shadow" >
               <a href={list.link} target="_blank" rel="noopener noreferrer">
                 <p>{list.title}</p>
               </a>
--- frontend/src/pages/MiddlePage.jsx
+++ frontend/src/pages/MiddlePage.jsx
@@ -5,11 +5,11 @@ import Banner from "../components/Banner";
 const MiddlePage = ({url}) => {
   return (
     <>
-      <main className="flex flex-col md:flex-row bg-[#f3f3f3] gap-5 md:gap-0">
-        <div className=" w-full md:w-1/2 flex justify-center items-center flex-col">
+      <main className="flex flex-col md:flex-row bg-[#f3f3f3] gap-5 md:gap-0 justify-between">
+        <div className=" w-full md:w-2/3 flex flex-col ">
           <UpdatedLink url={url}/>
         </div>
-        <div div className=" w-full md:w-1/2 flex justify-center items-center flex-col">
+        <div div className=" w-full md:w-1/3 flex justify-center items-center flex-col">
           <Banner url={url}/>
         </div>
       </main>
